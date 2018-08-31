b493635325e86631d7feabae69f9b91d356e54bc
Andre Tampubolon <andre@lc.vlsm.org>
2018-08-31 10:54:55 +0700

    Fixed incorrect commit

---
+++ app/src/main/java/app/anta40/com/osmdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package app.anta40.com.osmdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+
+public class MainActivity extends AppCompatActivity {
+
+    MapView map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        GeoPoint startPoint = new GeoPoint(48.13, -1.63);
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        mapController.setCenter(startPoint);
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        map.getOverlays().add(startMarker);
+
+        map.invalidate();
+
+        startMarker.setIcon(getResources().getDrawable(R.drawable.baseline_my_location_black_48dp));
+        startMarker.setTitle("Start point");
+    }
+}
